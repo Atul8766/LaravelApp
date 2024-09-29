b05769ea3443f61b8fc55db6165495ab1d35a831
Atul8766 <atul61541@gmail.com>
2024-09-29 23:40:58 +0530

    whatsapp check

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WhatsappController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/getUsers", [UserController::class, 'getUsers']);
+Route::get("/getUser", [UserController::class, 'getUsers']);
+Route::get('/whatsapp_page', [WhatsAppController::class, 'index']);
+Route::post('whatsapp', [WhatsAppController::class, 'store']);
+
+
